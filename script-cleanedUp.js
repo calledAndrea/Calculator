7b06c7004341335fdc7f989828b29a4d6fff4836
astrolitelinux <andreaxo47@gmail.com>
2020-01-25 13:50:10 -0500

    Consensed if statements by ACTION

--- script-cleanedUp.js
+++ script-cleanedUp.js
@@ -1,25 +1,39 @@
-const calculator = document.querySelector('.container');
+// removed all comments from draft file because code 
+// may change a lot. 
 
+//refactoring is basically cleaning up code. 
+// Following a tutorial claiming Javascript Best Practices
+
+const calculator = document.querySelector('.container');
 
 const display = calculator.querySelector('.result');
 
 const keys = calculator.querySelector('.button_wrapper');
 
-const calculate = (n1, operator, n2) => {
-    let result = '';
-    if (operator === 'add') {
-        result = parseFloat(n1) + parseFloat(n2);
-    } else if (operator === 'minus') {
-        result = parseFloat(n1) - parseFloat(n2);
-    } else if (operator === 'multiply') {
-        result = parseFloat(n1) * parseFloat(n2);
-    } else if (operator === 'divide') {
-        result = parseFloat(n1) / parseFloat(n2);
-    }
 
-    return result;
+// Avoid too much reassignment when you can. Using
+// early returns, which returns the result of a conditional
+// statement can be returned as soon as a result is available, 
+// rather than wait until the rest of the function is run
+// this also allows us to get rid of else if's
+const calculate = (n1, operator, n2) => {
+   // parse your values before going into if statments
+   // so you don't have to parse every time.
+   firstNum = parseFloat(n1);
+   secondNum = parseFloat(n2);
+
+    if (operator === 'add') { return (firstNum) + (secondNum); }
+        
+    if (operator === 'minus') { return (firstNum) - (secondNum); } 
+        
+    if (operator === 'multiply') { return (firstNum) * (secondNum); }
+    
+    if (operator === 'divide') {return (firstNum) / (secondNum); }
 }
 
+
+// This eventListener function is HUGE. Going to try refactoring by
+// splitting it into pure and inpure function
 keys.addEventListener('click', e => {
     
     if (e.target.matches('button')) {
@@ -40,7 +54,8 @@ keys.addEventListener('click', e => {
 
                      
         Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
-
+          
+        // Try to condense if statemnts below so theres only 1 main one for each action.
 
         if (!action) {
             if (displayedNum === '0' || previous === 'calculate') {
@@ -72,93 +87,86 @@ keys.addEventListener('click', e => {
                 console.log('0.');
                 calculator.dataset.previousKeyType = 'decimal';
 
-
             }
     
         }
 
-        if ((
+        if (
             action === 'add' ||
             action === 'minus' ||
             action === 'multiply' ||
-            action === 'divide') && (previous !== 'operator' && previous !== 'calculate')) {
+            action === 'divide') {
+            
+            if (previous !== 'operator' && previous !== 'calculate') {
 
-            const firstValue = calculator.dataset.firstValue;
-            const operator = calculator.dataset.operator;
-           
-            const secondValue = displayedNum.split(/[+\*\/ -]/).pop();
+             const firstValue = calculator.dataset.firstValue;
+             const operator = calculator.dataset.operator;
+            
+             const secondValue = displayedNum.split(/[+\*\/ -]/).pop();
 
-            if (firstValue && operator) {
-                const currentResult = calculate(firstValue, operator, secondValue);
-
-
-                console.log(firstValue + operator + secondValue);
-                console.log(currentResult);
-                console.log(keyContent);
-             
-                calculator.dataset.firstValue = currentResult;
+                if (firstValue && operator) {
+                    const currentResult = calculate(firstValue, operator, secondValue);
 
 
+                    console.log(firstValue + operator + secondValue);
+                    console.log(currentResult);
+                    console.log(keyContent);
+                    
+                    calculator.dataset.firstValue = currentResult;
 
-            } else {
-              
+
+
+                } else {
+                    
+                    // this is the display BEFORE the operator was pressed,
+                    // because we haven't updated it yet
+                    calculator.dataset.firstValue = displayedNum;
+
+                    console.log(keyContent);
+
+                } 
+                
+                calculator.dataset.previousKeyType = 'operator';
+
+
+                calculator.dataset.operator = action;
+
+
+                display.textContent = displayedNum + keyContent;
+            }
+
+            if (previous === 'calculate') {
+
+
                 calculator.dataset.firstValue = displayedNum;
 
+
+                calculator.dataset.operator = action;
+
+
+                display.textContent = displayedNum + keyContent;
+
                 console.log(keyContent);
+
+                calculator.dataset.previousKeyType = 'operator';
 
             }
 
+            if (previous === 'operator' || previous === 'decimal') {
+
+                display.textContent = displayedNum;
+
+                console.log('invalid: operator pressed ' + keyContent);
+            }
             
             key.classList.add('pressed');
 
-          
-            calculator.dataset.previousKeyType = 'operator';
-
-           
-            calculator.dataset.operator = action;
-
-            
-            display.textContent = displayedNum + keyContent;
-
-
         }
-
-        if ((
-            action === 'add' ||
-            action === 'minus' ||
-            action === 'multiply' ||
-            action === 'divide') && (previous === 'operator' || previous === 'decimal')) {
-
-            display.textContent = displayedNum;
-
-            console.log('invalid: operator pressed ' + keyContent);
-        }
-
-        if ((
-            action === 'add' ||
-            action === 'minus' ||
-            action === 'multiply' ||
-            action === 'divide') && (previous === 'calculate')) {
-
-          
-            calculator.dataset.firstValue = displayedNum;
-
-           
-            calculator.dataset.operator = action;
-
-            
-            display.textContent = displayedNum + keyContent;
-
-            console.log(keyContent);
-
-            calculator.dataset.previousKeyType = 'operator';
-
-        }
-
-
-
-        
-        if (action === 'calculate' && previous === 'operator') {
+       
+       
+        if (action === 'calculate'){
+            if (previous === 'operator') {
+                
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = displayedNum;
@@ -166,28 +174,29 @@ keys.addEventListener('click', e => {
 
             console.log(keyContent);
             console.log(firstValue, operator, secondValue);
-
+            
+            display.textContent = calculate(firstValue, operator, secondValue);
             
             if (firstValue) {
                 display.textContent = calculate(firstValue, operator, secondValue);
             }
 
             calculator.dataset.previousKeyType = 'calculate';
-        }
+           }
 
-        if (action === 'calculate' && (previous === 'number' || previous === 'backspace')) {
+           if (previous === 'number' || previous === 'backspace') {
 
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
-            
+
 
             console.log(keyContent);
-         
+
             if (firstValue) {
                 const secondValue = displayedNum.split(/[+\*\/ -]/).pop();
                 display.textContent = calculate(firstValue, operator, secondValue);
 
-               
+
                 currentResult = calculate(firstValue, operator, secondValue);
                 console.log(currentResult);
 
@@ -196,20 +205,18 @@ keys.addEventListener('click', e => {
 
                 calculator.dataset.modValue = secondValue;
 
-              
+
                 display.dataset.runningTotal = currentResult;
 
-            
+
             } else { console.log('invalid key pressed, nothing to calc yet') }
 
-            calculator.dataset.previousKeyType = 'calculate';
-        }
+          }
+          if  (previous === 'calculate') {
 
-        if (action === 'calculate' && previous === 'calculate') {
-           
             display.dataset.runningTotal = displayedNum;
 
-            
+
             const firstValue = display.dataset.runningTotal;
             const operator = calculator.dataset.operator;
             const secondValue = calculator.dataset.modValue;
@@ -221,8 +228,8 @@ keys.addEventListener('click', e => {
 
 
             calculator.dataset.previousKeyType = 'calculate';
+            }
         }
-
 
         if (action === 'clear') {
             console.log('clear key was pressed');
